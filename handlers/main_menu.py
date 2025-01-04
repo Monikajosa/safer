f448dfd0411fbcbf0e14060b65ee0f8e5788ab9a
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2025-01-04 20:01:59 +0100

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -2,17 +2,16 @@ from telegram import Update
 from telegram.ext import ContextTypes, ConversationHandler
 from .report import start_report
 from .check import start_check
-from i18n_helpers import generate_i18n_object
 
 async def handle_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    i18n = generate_i18n_object(update)
     text = update.message.text
-    if text == i18n.translate("report_scammer"):
+    if text == "Scammer melden":
         return await start_report(update, context, "scammers")
-    elif text == i18n.translate("report_trust"):
+    elif text == "Trust melden":
         return await start_report(update, context, "trusted")
-    elif text == i18n.translate("check_user"):
+    elif text == "User prüfen":
         return await start_check(update, context)
-    elif text == i18n.translate("request_deletion"):
-        await update.message.reply_text(i18n.translate("enter_id_and_reason"))
+    elif text == "Löschung beantragen":
+        await update.message.reply_text(
+            "Bitte geben Sie die ID und den Grund für die Löschung in folgendem Format ein:\n\nID: Grund")
         return ConversationHandler.END
