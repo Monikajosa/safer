08f9c5ec7ac6a19dd2d0aef5464ee1378dfc3c30
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2025-01-04 19:57:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Laden des Tokens aus Umgebungsvariablen oder direkt setzen
-TOKEN = os.getenv('TELEGRAM_TOKEN', 'your-telegram-bot-token')
-SUPPORT_GROUP_ID = os.getenv('SUPPORT_GROUP_ID', 'your-support-group-id')
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+SUPPORT_GROUP_ID = os.getenv("SUPPORT_GROUP_ID")
 OWNER_ID = os.getenv("OWNER_ID")
