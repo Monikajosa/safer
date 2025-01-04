b6ca520f36e62cf1d6b727014dcc0540953620fe
Monikajosa <59704999+Monikajosa@users.noreply.github.com>
2025-01-04 20:01:46 +0100

    Update error_handler.py

--- handlers/error_handler.py
+++ handlers/error_handler.py
@@ -2,15 +2,13 @@ import logging
 from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.error import TimedOut
-from i18n_helpers import generate_i18n_object
 
 logger = logging.getLogger(__name__)
 
 async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    i18n = generate_i18n_object(update)
     try:
-        logger.error(msg=i18n.translate("exception_while_handling_update"), exc_info=context.error)
-        await update.message.reply_text(i18n.translate("unexpected_error_occurred"))
+        logger.error(msg="Exception while handling an update:", exc_info=context.error)
+        await update.message.reply_text("Ein unerwarteter Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.")
     except TimedOut:
-        logger.error(i18n.translate("request_timed_out_retrying"))
+        logger.error("Request timed out. Retrying...")
         # Hier können Sie Logik hinzufügen, um die Anfrage erneut zu senden oder andere Maßnahmen zu ergreifen.
